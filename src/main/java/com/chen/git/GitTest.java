ff061bfa0748ee96883492c39a7848a19c4f1f64
Chen <CC@chen.com>
2022-05-03 09:53:58 +0800

    second commit

--- src/main/java/com/chen/git/GitTest.java
+++ src/main/java/com/chen/git/GitTest.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
