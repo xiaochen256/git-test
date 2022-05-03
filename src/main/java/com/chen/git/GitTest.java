43b33323eef60b854d57d2b21b423f1563382baf
Chen <CC@chen.com>
2022-05-03 09:47:20 +0800

    first commit

---
+++ src/main/java/com/chen/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.chen.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
